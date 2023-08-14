85ff30ee35ddf7f8bb82396ff962c42e36103354
lekhalinh <lekhalinh.oppo@gmail.com>
2023-08-14 14:04:03 +0700

    edit source mac

--- API/Program.cs
+++ API/Program.cs
@@ -1,13 +1,4 @@
-using System.Runtime.Intrinsics.Arm;
-using System.Text;
-using API.Controllers;
-using API.Data;
 using API.Extensions;
-using API.Interfaces;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
